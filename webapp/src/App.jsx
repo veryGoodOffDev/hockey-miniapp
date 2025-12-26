d9d5e63412a9d43da8521f4d2e93f9e1719ea1f9
VeryGoodOffDev <96524370+veryGoodOffDev@users.noreply.github.com>
2025-12-27 01:16:25 +0300

    Update App.jsx

--- webapp/src/App.jsx
+++ webapp/src/App.jsx
@@ -801,7 +801,6 @@ const teamsStaleInfo = useMemo(() => {
                     const bgUrl = GAME_BGS[idx % GAME_BGS.length];
 
                     return (
-                      <div className="gameCardContent">
                             <div
                                 key={g.id}
                                 className={`card gameCard ${tone} status-${status} ${isNext ? "isNext" : ""} ${past ? "isPast" : ""}`}
@@ -883,7 +882,6 @@ const teamsStaleInfo = useMemo(() => {
                           </button>
                         </div>
                       </div>
-                    </div>
                     );
                   })}
                 </div>
