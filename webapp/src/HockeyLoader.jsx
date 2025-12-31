0b404edf10b08c35b328fe77187042a45025891a
unknown <iluha.kot@gmail.com>
2025-12-31 21:30:11 +0300

    changed loader

--- webapp/src/HockeyLoader.jsx
+++ webapp/src/HockeyLoader.jsx
@@ -1,51 +1,90 @@
-import { useEffect, useState } from "react";
 import logo from "./logoo.jpg";
+
 export default function HockeyLoader({ text = "Загрузка..." }) {
-  const [direction, setDirection] = useState("right"); // 'left' | 'right'
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setDirection((prev) => (prev === "right" ? "left" : "right"));
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, []);
-
   return (
-    <div className="hkLoader">
-      <div className="hkStage">
-        <div className="hkSticks">
-          <div className={`hkStickWrap ${direction === "left" ? "isActive" : ""}`}>
-            <svg width="80" height="80" viewBox="0 0 36 36" xmlns="http://www.w3.org/2000/svg" className="hkStickSvg">
-              <path d="M36 3c-1 1.295-3.027 3.803-4.391 5.671c-3.816 5.225-7.156 6.454-10.328 7.632c-3.172 1.178-10.407 1.029-13.88.854c-3.473-.175-5.735-.579-6.42.415c-2.102 3.053-.612 6.481 2.426 6.949c2.378.366 9.544-.32 12.899-.616c3.356-.297 7.024-1.301 8.283-1.785c1.259-.483 2.279-.88 2.597-1.644c.318-.765 1.876-2.817 3.783-5.917C32.045 12.811 35 9.55 36 8V3z"></path>
-            </svg>
-          </div>
-          {/* LOGO */}
-          <div className="hkLogoWrap" aria-hidden="true">
-            <img className="hkLogo" src={logo} alt="Team logo" />
-          </div>
-          <div className={`hkStickWrap hkRight ${direction === "right" ? "isActive" : ""}`}>
-            <svg width="80" height="80" viewBox="0 0 36 36" xmlns="http://www.w3.org/2000/svg" className="hkStickSvg hkFlip">
-              <path d="M36 3c-1 1.295-3.027 3.803-4.391 5.671c-3.816 5.225-7.156 6.454-10.328 7.632c-3.172 1.178-10.407 1.029-13.88.854c-3.473-.175-5.735-.579-6.42.415c-2.102 3.053-.612 6.481 2.426 6.949c2.378.366 9.544-.32 12.899-.616c3.356-.297 7.024-1.301 8.283-1.785c1.259-.483 2.279-.88 2.597-1.644c.318-.765 1.876-2.817 3.783-5.917C32.045 12.811 35 9.55 36 8V3z"></path>
-            </svg>
-          </div>
+    <div className="hkLoader2">
+      <div className="hkOrbitStage" aria-label="Loading">
+        {/* центральный медальон */}
+        <div className="hkMedal">
+          <img className="hkMedalLogo" src={logo} alt="Team logo" />
         </div>
 
-        <div className="hkPuckRail">
-          <div className={`hkPuck ${direction === "right" ? "toRight" : "toLeft"}`} />
+        {/* орбита */}
+        <div className="hkOrbit">
+          {/* шайба */}
+          <div className="hkPuck2" />
+
+          {/* небольшой след/блик — опционально */}
+          <div className="hkPuckTrail" />
         </div>
 
-        <div className="hkIceLine" />
+        {/* декоративные линии “льда” */}
+        <div className="hkIceRings" aria-hidden="true">
+          <div className="hkRing hkRing1" />
+          <div className="hkRing hkRing2" />
+        </div>
       </div>
 
-      <div className="hkMeta">
-        <div className="hkDots">
+      <div className="hkMeta2">
+        <div className="hkDots2">
           <span />
           <span />
           <span />
         </div>
-        <div className="hkText">{text}</div>
+        <div className="hkText2">{text}</div>
       </div>
     </div>
   );
 }
+
+// import { useEffect, useState } from "react";
+// import logo from "./logoo.jpg";
+// export default function HockeyLoader({ text = "Загрузка..." }) {
+//   const [direction, setDirection] = useState("right"); // 'left' | 'right'
+
+//   useEffect(() => {
+//     const interval = setInterval(() => {
+//       setDirection((prev) => (prev === "right" ? "left" : "right"));
+//     }, 1000);
+
+//     return () => clearInterval(interval);
+//   }, []);
+
+//   return (
+//     <div className="hkLoader">
+//       <div className="hkStage">
+//         <div className="hkSticks">
+//           <div className={`hkStickWrap ${direction === "left" ? "isActive" : ""}`}>
+//             <svg width="80" height="80" viewBox="0 0 36 36" xmlns="http://www.w3.org/2000/svg" className="hkStickSvg">
+//               <path d="M36 3c-1 1.295-3.027 3.803-4.391 5.671c-3.816 5.225-7.156 6.454-10.328 7.632c-3.172 1.178-10.407 1.029-13.88.854c-3.473-.175-5.735-.579-6.42.415c-2.102 3.053-.612 6.481 2.426 6.949c2.378.366 9.544-.32 12.899-.616c3.356-.297 7.024-1.301 8.283-1.785c1.259-.483 2.279-.88 2.597-1.644c.318-.765 1.876-2.817 3.783-5.917C32.045 12.811 35 9.55 36 8V3z"></path>
+//             </svg>
+//           </div>
+//           {/* LOGO */}
+//           <div className="hkLogoWrap" aria-hidden="true">
+//             <img className="hkLogo" src={logo} alt="Team logo" />
+//           </div>
+//           <div className={`hkStickWrap hkRight ${direction === "right" ? "isActive" : ""}`}>
+//             <svg width="80" height="80" viewBox="0 0 36 36" xmlns="http://www.w3.org/2000/svg" className="hkStickSvg hkFlip">
+//               <path d="M36 3c-1 1.295-3.027 3.803-4.391 5.671c-3.816 5.225-7.156 6.454-10.328 7.632c-3.172 1.178-10.407 1.029-13.88.854c-3.473-.175-5.735-.579-6.42.415c-2.102 3.053-.612 6.481 2.426 6.949c2.378.366 9.544-.32 12.899-.616c3.356-.297 7.024-1.301 8.283-1.785c1.259-.483 2.279-.88 2.597-1.644c.318-.765 1.876-2.817 3.783-5.917C32.045 12.811 35 9.55 36 8V3z"></path>
+//             </svg>
+//           </div>
+//         </div>
+
+//         <div className="hkPuckRail">
+//           <div className={`hkPuck ${direction === "right" ? "toRight" : "toLeft"}`} />
+//         </div>
+
+//         <div className="hkIceLine" />
+//       </div>
+
+//       <div className="hkMeta">
+//         <div className="hkDots">
+//           <span />
+//           <span />
+//           <span />
+//         </div>
+//         <div className="hkText">{text}</div>
+//       </div>
+//     </div>
+//   );
+// }
