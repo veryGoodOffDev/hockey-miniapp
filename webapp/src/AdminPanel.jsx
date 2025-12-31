0525784f289a78ecaf3576e6a7760ccecc2f5fb6
unknown <iluha.kot@gmail.com>
2025-12-31 18:18:39 +0300

    fix map geo styles

--- webapp/src/AdminPanel.jsx
+++ webapp/src/AdminPanel.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useMemo, useRef, useState } from "react";
-import { MapContainer, TileLayer, Marker, useMapEvents } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, useMapEvents, useMap } from "react-leaflet";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
