8bf05d035043610dab048a5ea1a288e8cf31de3c
VeryGoodOffDev <96524370+veryGoodOffDev@users.noreply.github.com>
2026-03-04 15:28:15 +0300

    Revert "Add PWA + Web Push for web app (unread badge + message pushes)"

--- webapp/src/main.jsx
+++ webapp/src/main.jsx
@@ -2,13 +2,9 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./styles.css";
-import { registerServiceWorker } from "./pwaPush.js";
 
 const tg = window.Telegram?.WebApp;
 tg?.ready?.();
 tg?.expand?.();
 
 createRoot(document.getElementById("root")).render(<App />);
-
-
-registerServiceWorker().catch(() => {});
